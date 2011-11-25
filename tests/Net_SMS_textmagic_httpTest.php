ff45a3017322dfaf9b642597db0109256230963c
Daniel O'Connor <daniel.oconnor@gmail.com>
2011-11-26 00:24:58 +1030

    Acutally send content

--- tests/Net_SMS_textmagic_httpTest.php
+++ tests/Net_SMS_textmagic_httpTest.php
@@ -10,7 +10,7 @@ class Net_SMS_textmagic_httpTest extends PHPUnit_Framework_TestCase {
 
         $driver->send(array(
             'id' => 0,
-            'to' => array(), 
+            'to' => array("hi"), 
             'text' => 'hi'
         ));
     }
